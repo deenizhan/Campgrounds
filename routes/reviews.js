a30a020c829eef1414cca6d4a8c7cdde8137faef
Denizhan Yılmaz <deenizhan@users.noreply.github.com>
2023-07-18 22:25:06 +0300

    flash setup completed

--- routes/reviews.js
+++ routes/reviews.js
@@ -25,6 +25,7 @@ router.post('/', validateReview, catchAsync(async (req, res) => {
     campground.reviews.push(review);
     await review.save();
     await campground.save();
+    req.flash('success', 'Successfully made a review.')
     res.redirect(`/campgrounds/${campground._id}`); // 64a41271cf2f1f833bd3266c
 }))
 
@@ -32,6 +33,7 @@ router.delete('/:reviewId', catchAsync(async (req, res) => {
     const { id, reviewId } = req.params;
     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } })
     await Review.findByIdAndDelete(reviewId);
+    req.flash('success', 'Successfully deleted!')
     res.redirect(`/campgrounds/${id}`)
 }))
 
