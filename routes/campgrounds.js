291884be25c68f12567abb58617dc684f1954d46
Denizhan Yılmaz <deenizhan@users.noreply.github.com>
2023-07-30 15:41:48 +0300

    image upload

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,24 +1,35 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
+// console.log(process.env);
+// console.log(process.env.NODE_ENV);
+// console.log(process.env.SECRET);
+// console.log(process.env.API_KEY);
+
 const express = require('express');
 const router = express.Router();
-const campgrounds = require('../controllers/campgrounds')
+const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utils/catchAsync');
-const Campground = require('../models/campground');
-const ExpressError = require('../utils/ExpressError')
-const passport = require('passport');
-const { campgroundSchema, reviewSchema } = require('../schemas.js')
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
+const multer = require('multer');
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
 
+const Campground = require('../models/campground');
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(validateCampground, isLoggedIn, catchAsync(campgrounds.createCampground))
+    // .post(validateCampground, isLoggedIn, catchAsync(campgrounds.createCampground))
+    .post(upload.array('image'), (req, res) => {
+        console.log(req.files)
+    })
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm)
 
 router.route('/:id')
     .get(catchAsync(campgrounds.showCampground))
-    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
-    .delete(isLoggedIn, catchAsync(campgrounds.deleteCampground))
+    .put(isLoggedIn, isAuthor, upload.array('image'), validateCampground, catchAsync(campgrounds.updateCampground))
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm))
 
